ab7a8312bb45c648b5b1e4dd51fc34a6fde0a0ce
Dalvis <Dalvis.liz27@gmail.com>
2023-07-24 18:35:38 -0400

    set up server middleware and session config

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-
+const session = require("express-session");
+const Store = require("connect-session-knex")(session);
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -20,6 +21,24 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session({
+  name: "chocolatechip",
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true,
+  }, 
+    resave: false,
+    saveUninitialized: false,
+    store: new Store({
+      knex: require("../data/db-config"),
+      tablename: "sessions",
+      sidfieldname: "sid",
+      createtable: true,
+      clearInterval: 1000 * 60 * 60,
+    }),
+}));
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
